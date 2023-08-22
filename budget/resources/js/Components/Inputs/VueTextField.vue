1f9c47cf57a092603a7bbbc13ba40aacb751a306
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-08-22 21:58:59 +1000

    Add validation error message and prevent login when invalid

--- budget/resources/js/Components/Inputs/VueTextField.vue
+++ budget/resources/js/Components/Inputs/VueTextField.vue
@@ -1,21 +1,32 @@
 <template>
 	<div class="mt-2 grid grid-cols-2">
-		<label :for="id" class="align-center" >{{ label }}</label>
-		<input
-			@input="$emit('update:text', text)"
-			v-model="text"
-			:disabled="disabled"
-			:type="type"
-			:id="id"
-			class="border-solid border border-zinc-400 rounded-sm p-0 disabled:bg-zinc-300"
-		/>
+		<label :for="id" class="self-center">{{ label }}</label>
+		<VField :type="type" :name="name" :rules="rules" v-slot="{ field, meta }">
+			<input
+				@input="$emit('update:text', text)"
+				v-model="text"
+				v-bind="field"
+				:disabled="disabled"
+				:type="type"
+				:id="id"
+				:class="[`
+					border-solid border border-zinc-400 rounded-sm p-1
+					disabled:bg-zinc-300
+				`, {'border-red-400': meta.dirty && !meta.valid}]"
+			/>
+		</VField>
+		<span></span>
+		<ErrorMessage class="text-red-500 text-xs italic" :name="name"></ErrorMessage>
 	</div>
 </template>
 <script setup lang="ts">
 import { defineComponent } from 'vue';
+import { Field, ErrorMessage } from 'vee-validate';
 
 withDefaults(defineProps<{
 	label: string,
+	name: string,
+	rules?: any,
 	disabled?: boolean,
 	type?: string
 }>(), {
@@ -26,6 +37,10 @@ withDefaults(defineProps<{
 <script lang="ts">
 export default defineComponent({
 	name: 'TextField',
+	components: {
+		VField: Field,
+		ErrorMessage
+	},
 	computed: {
 		id() {
 			return `${crypto.randomUUID()}--textfield`;
--- budget/resources/js/Components/Login/LoginDialog.vue
+++ budget/resources/js/Components/Login/LoginDialog.vue
@@ -4,6 +4,7 @@ import ConfirmButton from '../Inputs/ConfirmButton.vue';
 import BasicDialog from '../BasicDialog.vue';
 import CancelButton from '../Inputs/CancelButton.vue';
 import VueTextField from '../Inputs/VueTextField.vue';
+import { Form } from 'vee-validate';
 
 const dialog = ref<InstanceType<typeof BasicDialog> | null>(null);
 defineExpose({ dialog })
@@ -14,43 +15,62 @@ import loginApi from '@/services/Login/loginApi';
 
 export default defineComponent({
     name: 'LoginDialog',
+    components: { ConfirmButton, BasicDialog, VueTextField, VForm: Form },
 	data() {
 		return {
 			username: '',
 			password: '',
 			is: {
-				loading: false
+				loading: false,
+			},
+			error: {
+				message: null,
+				active: false
 			}
 		}
 	},
     methods: {
-		async login() {
+		async login(valid = true) {
+			if (!valid) {
+				return;
+			}
 			this.is.loading = true;
+			this.error.active = false;
 			const response = await loginApi.login(this.username, this.password);
 
 			if (response.status === 200) {
 				window.location.href = route('dashboard');
-				console.log('logged in');
 			} else {
-				console.error('uh oh');
+				this.error.active = true;
+				this.error.message = response.data.message;
 			}
 			this.is.loading = false;
+		},
+		validateUsername(value: string) {
+			return value?.length < 5 ? 'Username must be 5 characters long' : true;
+		},
+		validatePassword(value: string) {
+			return value?.length < 5 ? 'Password must be 5 characters long' : true;
 		}
-    },
-    components: { ConfirmButton, BasicDialog, VueTextField }
+    }
 })
 </script>
 
 <template>
 	<BasicDialog ref="dialog" class="isolate sticky">
-		<div class="grid grid-cols-1">
-			<VueTextField @update:text="(n) => username = n" label="Username:" :disabled="is.loading" v-on:keyup.enter="login"/>
-			<VueTextField @update:text="(n) => password = n" label="Password:" type="password" :disabled="is.loading" v-on:keyup.enter="login"/>
-		</div>
+		<VForm ref="form" v-slot="{ meta }">
+			<div class="grid grid-cols-1">
+				<VueTextField :rules="validateUsername" name="username" @update:text="(n) => username = n" label="Username:" :disabled="is.loading" v-on:keyup.enter="login(meta.dirty && meta.valid)"/>
+				<VueTextField :rules="validatePassword" name="password" @update:text="(n) => password = n" label="Password:" type="password" :disabled="is.loading" v-on:keyup.enter="login(meta.dirty && meta.valid)"/>
+			</div>
 
-		<div class="flex flex-row mt-auto">
-			<ConfirmButton @click="login" label="Login" />
-			<CancelButton @click="dialog?.hide" label="Cancel" />
-		</div>
+			<div class="flex flex-row mt-auto">
+				<ConfirmButton @click="login" label="Login" />
+				<CancelButton @click="dialog?.hide" label="Cancel" />
+			</div>
+		</VForm>
+		<span v-if="error.active" class="italic text-red-500 text-xs">
+			{{ error.message }}
+		</span>
 	</BasicDialog>
 </template>
