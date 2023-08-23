b5ea79ad83e32a9b7826bd13f564ff707b837ef7
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-08-24 00:15:42 +1000

    Install sass

---
+++ budget/resources/js/Components/Receipts/ReceiptTable.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="p-4">
+		<div class="table-header">
+			<span class="table-header-text">Reference Number</span>
+			<span class="table-header-text">Store</span>
+			<span class="table-header-text">Location</span>
+			<span class="table-header-text">Cost</span>
+			<span class="table-header-text">Category</span>
+		</div>
+		<div v-for="receipt of $page.props.receipts" class="table-row">
+			<span>{{ receipt.reference }}</span>
+			<span>{{ receipt.store }}</span>
+			<span>{{ receipt.location }}</span>
+			<span>${{ receipt.cost }}</span>
+			<span>{{ receipt.category }}</span>
+		</div>
+	</div>
+</template>
+<style lang="scss" scoped>
+.table-header {
+	@apply grid grid-cols-5 font-semibold text-center;
+	&-text {
+		@apply cursor-pointer hover:bg-slate-200;
+	}
+}
+
+.table-row {
+	@apply grid grid-cols-5 cursor-pointer hover:bg-slate-100 text-center;
+}
+</style>
---
+++ budget/resources/js/Pages/Receipts/ReceiptList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import { Head } from '@inertiajs/vue3';
+import HeaderBar from '@/Components/Navigation/HeaderBar.vue';
+import ReceiptTable from '@/Components/Receipts/ReceiptTable.vue';
+</script>
+<script lang="ts">
+export default defineComponent({
+	name: 'ReceiptList',
+	components: {Head, HeaderBar, ReceiptTable}
+})
+</script>
+
+<template>
+	<Head title="Receipts" />
+	<HeaderBar />
+
+	<ReceiptTable />
+</template>
