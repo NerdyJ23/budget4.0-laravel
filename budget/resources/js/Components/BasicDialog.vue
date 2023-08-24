5111a5d1782004fed05af45670138e05ed353a8a
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-08-24 16:21:03 +1000

    Move receipt dialog into own folder

--- budget/resources/js/Components/BasicDialog.vue
+++ budget/resources/js/Components/BasicDialog.vue
@@ -1,46 +1,43 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { defineComponent, ref, reactive } from 'vue';
+const props = defineProps<{
+	persistent?: boolean,
+	blur?: boolean
+}>();
 
-export default defineComponent({
-    name: 'BasicDialog',
-	props: {
-		persistent: {
-			type: Boolean,
-			required: false,
-			default: false
-		}
-	},
-    data() {
-        return {
-            is: {
-                open: false,
-				shake: false
-            }
-        };
-    },
-    methods: {
-        show(): void {
-            this.is.open = true;
-        },
-        hide(): void {
-            this.is.open = false;
-        },
-		hideSelf() {
-			this.is.shake = false;
-			if (!this.persistent) {
-				this.hide();
-			} else {
-				this.is.shake = true;
-				setTimeout(() => this.is.shake = false, 1000);
-			}
-		}
-    },
-})
+const is = reactive({
+	open: false,
+	shake: false
+});
+
+const show = () => { is.open = true; }
+const hide = () => { is.open = false; }
+const hideSelf = () => {
+	is.shake = false;
+	if (!props.persistent) {
+		hide();
+	} else {
+		is.shake = true;
+		setTimeout(() => is.shake = false, 1000);
+	}
+}
+defineExpose({is, show, hide, hideSelf});
+</script>
+<script lang="ts">
+export default defineComponent({name: 'BasicDialog'})
 </script>
 
 <template>
-	<div v-if="is.open" class="w-full bg-neutral-600/60 absolute object-center inset-0" @click.self="hideSelf">
+	<div
+		v-if="is.open"
+		:class="[
+			{'backdrop-blur-sm': blur},
+			`w-full bg-neutral-600/60 absolute object-center inset-0`
+		]"
+		@click.self="hideSelf"
+	>
 		<dialog
+			v-bind="$attrs"
 			:open="is.open"
 			:class="[
 				{'animate-shake-horizontal shadow-red-600': is.shake},
--- budget/resources/js/Components/Receipts/Dialog/ReceiptDialog.vue
+++ budget/resources/js/Components/Receipts/Dialog/ReceiptDialog.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { ReceiptItem } from '@/types/Receipts/receiptItem';
-import BasicDialog from '../BasicDialog.vue';
 import { defineComponent, ref } from 'vue';
+
+import BasicDialog from '@/Components/BasicDialog.vue';
 import { Form } from 'vee-validate';
 import VueTextField from '@/Components/Inputs/VueTextField.vue';
+import ReceiptDialogItem from './ReceiptDialogItem.vue';
+
 import { addIcons } from "oh-vue-icons";
 import { IoCloseOutline } from "oh-vue-icons/icons";
 
@@ -13,6 +16,7 @@ addIcons(IoCloseOutline);
 defineExpose({dialog});
 </script>
 <script lang="ts">
+
 export default defineComponent({
 	name: 'ReceiptDialog',
 	components: {
@@ -23,15 +27,25 @@ export default defineComponent({
 });
 </script>
 <template>
- <BasicDialog ref="dialog" persistent>
+ <BasicDialog ref="dialog" persistent blur>
 	<div class="flex flex-row">
 		<span class="header-text mr-auto">Create Receipt</span>
 		<span class="icon-button hover:animate-hop-once" @click="($refs.dialog as typeof BasicDialog).hide()">
-			<VIcon name="io-close-outline" label="Close"></VIcon>
+			<VIcon name="io-close-outline" label="Close" scale="1.3"></VIcon>
 		</span>
 	</div>
-	<VForm>
-		<VueTextField label="test" name="test" />
-	</VForm>
+	<div>
+		<VForm>
+			<div class="inline-flex flex-row">
+				<VueTextField placeholder="Receipt Number" name="receiptnumber" />
+				<VueTextField placeholder="Store Name" name="store" />
+				<VueTextField placeholder="Location" name="location" />
+			</div>
+			<div class="flex flex-col">
+				<ReceiptDialogItem></ReceiptDialogItem>
+			</div>
+		</VForm>
+	</div>
+
  </BasicDialog>
 </template>
