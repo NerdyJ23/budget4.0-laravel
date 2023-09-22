522698c10b71a5406f6e32f882ddb809b28ed9ec
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-09-22 21:03:42 +1000

    Add mobile friendly setting to cancel/confirm

--- budget/resources/js/Components/Inputs/CancelButton.vue
+++ budget/resources/js/Components/Inputs/CancelButton.vue
@@ -1,12 +1,22 @@
 <template>
-	<input
-		type="button"
+	<div
 		:value="label"
-		:class="`hover:cursor-pointer px-2 rounded-sm select-none
+		:class="`hover:cursor-pointer px-1 md:px-2 rounded-full md:rounded-sm select-none
 		border-solid border border-red-500 hover:bg-red-200`"
-	/>
+	>
+	<slot>
+		<div class="hidden md:inline">
+			Cancel
+		</div>
+		<div class="inline md:hidden">
+			<VIcon name="io-close" scale="1.5" class="fill-red-600"></VIcon>
+		</div>
+	</slot>
+	</div>
 </template>
 <script setup lang="ts">
+import { IoClose } from "oh-vue-icons/icons";
+import { addIcons } from "oh-vue-icons";
 import { defineComponent } from 'vue';
 
 withDefaults(defineProps<{
@@ -15,6 +25,7 @@ withDefaults(defineProps<{
 }>(), {
 	label: 'Cancel'
 });
+addIcons(IoClose)
 </script>
 
 <script lang="ts">
--- budget/resources/js/Components/Inputs/ConfirmButton.vue
+++ budget/resources/js/Components/Inputs/ConfirmButton.vue
@@ -1,14 +1,22 @@
 <template>
 <div
 	v-bind="$attrs"
-	:class="`hover:cursor-pointer px-2 rounded-sm select-none
-	bg-lime-600/80 hover:bg-lime-600
+	:class="`hover:cursor-pointer px-1 md:px-2 md:rounded-sm select-none
+	md:bg-lime-600/80 hover:bg-lime-600 border border-lime-600 rounded-full
 	`"
 >
-	<slot>Confirm</slot>
-</div>
+	<slot>
+		<div class="hidden md:inline">
+			Confirm
+		</div>
+		<div class="inline md:hidden">
+			<VIcon name="pr-save" scale="1.5" class="fill-lime-600"></VIcon>
+		</div>
+	</slot></div>
 </template>
 <script setup lang="ts">
+import { PrSave } from "oh-vue-icons/icons";
+import { addIcons } from "oh-vue-icons";
 import { defineComponent } from 'vue';
 
 withDefaults(defineProps<{
@@ -17,6 +25,7 @@ withDefaults(defineProps<{
 }>(), {
 	label: 'Confirm'
 });
+addIcons(PrSave);
 </script>
 
 <script lang="ts">
