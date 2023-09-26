5be2e41b578fe8ade6480d25d4ca8243b93b0b58
NerdyJ23 <jessica.moolenschot@outlook.com>
2023-09-26 19:46:11 +1000

    Fix pre-render optimisation not allowing dynamic class to work

--- budget/tailwind.config.js
+++ budget/tailwind.config.js
@@ -9,7 +9,9 @@ export default {
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
     ],
-
+	safelist: [{
+		pattern: /grid-cols-./
+	}],
     theme: {
         extend: {
             fontFamily: {
